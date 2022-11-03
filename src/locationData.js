55d344090ebc8852f2ee294265f0b11deb598435
Daniel Hjartland <squashical@gmail.com>
2022-11-03 19:17:26 +0100

    Create locationData and weatherData objects for data from API

---
+++ src/locationData.js
@@ -0,0 +1,3 @@
+const locationData = (name, lat, lon, country) => {
+  return { name, lat, lon, country };
+}
---
+++ src/weatherData.js
@@ -0,0 +1,17 @@
+const weatherData = (
+  weatherDescription,
+  temperature,
+  feelsLike,
+  humidity,
+  chanceOfRain,
+  windSpeed
+) => {
+  return {
+    weatherDescription,
+    temperature,
+    feelsLike,
+    humidity,
+    chanceOfRain,
+    windSpeed,
+  };
+};
