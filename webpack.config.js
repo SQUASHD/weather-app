388d35c48cb1d66164f18be17b4a3a8d329fb078
Daniel Hjartland <squashical@gmail.com>
2022-11-04 13:19:09 +0100

    Change from development to production

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,10 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'production',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  mode: 'development',
-  devtool: 'inline-source-map',
 };
